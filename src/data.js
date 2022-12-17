ad5dbedbbdc0fd0364ee6280ed48c41317ecf11a
Luu Binh An <an.luu@messagemedia.com>
2022-12-17 16:28:12 +0700

    init comit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        a: 'Considerate',
+        b: 'Decisive',
+        c: 'Logical',
+        d: 'Outgoing'
+    },
+    {
+        a: 'Friendly to others',
+        b: 'Results driven',
+        c: 'Thorough',
+        d: 'Spontaneous'
+    },
+    {
+        a: 'Thoughtful of others',
+        b: 'Daring',
+        c: 'Wants all information',
+        d: 'Persuasive'
+    },
+    {
+        a: 'Giving',
+        b: 'Independent',
+        c: 'Organised',
+        d: 'Articulate'
+    },
+    {
+        a: 'Reliable',
+        b: 'Risk taker',
+        c: 'Wants things exact',
+        d: 'Encouraging'
+    },
+    {
+        a: 'Friendly',
+        b: 'Persistent',
+        c: 'Likes routines',
+        d: 'Influential'
+    },
+    {
+        a: 'Calmly listens',
+        b: 'Wants to win',
+        c: 'Deliberate',
+        d: 'Optimistic'
+    },
+    {
+        a: 'Empathetic',
+        b: 'Competitive',
+        c: 'Perfectionist',
+        d: 'Gets others involved'
+    },
+    {
+        a: 'Patient',
+        b: 'Courageous',
+        c: 'Systematic',
+        d: 'Creative'
+    },
+    {
+        a: 'Steady and stable',
+        b: 'Bold',
+        c: 'Cautious',
+        d: 'Enthusiastic'
+    },
+]
+
+export default data
---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <h1 class="text-2xl mb-2">
+      The following questionnaire will enable you to reflect on how you think
+      you behave and will give an indication of your behavioural style.
+    </h1>
+    <div>There are no right and wrong responses!</div>
+    <div>
+      Consider each group of four words or descriptive phrases A to D. Select
+      the most appropriate number in the drop down menu next to each word or
+      descriptive phrase. Allocate a 4 to the word or phrase that is most like
+      you or that you relate to most. Allocate a 3 to the next most like you,
+      then a 2 and then a 1 to the least like you.
+    </div>
+    <div>
+      Make sure each group of words or descriptive phrases (A to D) has a 4, 3, 2 and 1 (<strong>no duplicates</strong>).
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Home",
+  },
+};
+</script>
